ff015c3d984d84fb4b811da0b16839a65e12c5a0
ShreyPinnada <shrey30000official@gmail.com>
2024-09-12 19:33:43 +0530

    All about variables changes in index.js

--- js-basics/index.js
+++ js-basics/index.js
@@ -1,2 +1,23 @@
-//comment should contain why and how rather than logging the string (its obvious)
-console.log('Hello World');
+//before es6 we used 'var' but it has some issues
+
+//going forward from es6 we use the 'let' keyword as the best practice
+
+let name = 'Shrey';
+console.log(name);
+
+//rules for variable names we already know
+// Cannot be a reserved keyword
+//Should be meaningful
+// Cannot start with a number
+// Cannot contain a space or hyphen
+// Case Sensititve
+
+//some valid variable declarations are given below:
+
+let firstName; //better to use Camel case
+
+//decalring multiple variables
+let first = 'shrey' , last = 'pinnada';
+//modern best practice is to have in seperate lines
+let firstN = 'shrey';
+let lastN = 'pinnada';
