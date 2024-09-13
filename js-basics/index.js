4c4fbfa1dfd045f6fd792913dacbd9b85ec0ce3f
ShreyPinnada <shrey30000official@gmail.com>
2024-09-13 19:57:40 +0530

    Functions

--- js-basics/index.js
+++ js-basics/index.js
@@ -4,17 +4,13 @@
 // Function
 
 
-//Arrays
-let selectedColors = ['red','blue']; // [] empty array [] is the array literal
-console.log(selectedColors[0]);
+//FUNCTIONS
 
-//in js the length and the type of objects in array are dynamic as js is a dynamic language
+function greet(name , lastName) {
+    console.log('Hello '+name +' '+lastName);
+}
 
-selectedColors[2] = 'green';
-console.log(selectedColors);
+greet('Shrey','Pinnada');
+greet('Shashank')
 
-selectedColors[3] = 1;
-console.log(selectedColors);
-
-//to see all properties in array in js just put a dot and it will show a list of operations available
-console.log(selectedColors.length);
+// if you dont values to the params you will get undefined as default
